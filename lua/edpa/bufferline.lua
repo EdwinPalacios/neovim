11e545ae468e8bc896b7d052477b3a03ef94303b
EdwinPalacios <epalacios13@outlook.com>
2022-04-24 19:29:26 -0500

    feat(bufferline): change function diagnostic indicator

--- lua/edpa/bufferline.lua
+++ lua/edpa/bufferline.lua
@@ -38,13 +38,8 @@ bufferline.setup {
     diagnostics = "nvim_lsp", -- | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
     diagnostics_indicator = function(count, level, diagnostics_dict, context)
-      local s = " "
-      for e, n in pairs(diagnostics_dict) do
-        local sym = e == "error" and " "
-          or (e == "warning" and " " or "" )
-        s = s .. n .. sym
-      end
-      return s
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
     end,
     -- NOTE: this will be called a lot so don't do any heavy processing here
     -- custom_filter = function(buf_number)
