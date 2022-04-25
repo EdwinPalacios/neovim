fd0786f5236f96ada30e8e4fe544713c29450d39
EdwinPalacios <epalacios13@outlook.com>
2022-04-25 00:34:19 -0500

    fix(lualine): fix name active lsp

--- lua/edpa/bufferline.lua
+++ lua/edpa/bufferline.lua
@@ -57,7 +57,7 @@ bufferline.setup {
     --     return true
     --   end
     -- end,
-    offsets = {{filetype = "NvimTree", text = "File Explorer", text_align = "left"}},
+    offsets = { { filetype = "NvimTree", text = "File Explorer", text_align = "left" } },
     color_icons = true, -- whether or not to add the filetype icon highlights
     show_buffer_icons = true, -- disable filetype icons for buffers
     show_buffer_close_icons = true,
--- lua/edpa/lualine.lua
+++ lua/edpa/lualine.lua
@@ -35,7 +35,7 @@ local diagnostics = {
 local lsp = {
 	-- Lsp server name .
 	function()
-		local msg = 'No ctive Lsp'
+		local msg = 'No active Lsp'
 		local buf_ft = vim.api.nvim_buf_get_option(0, 'filetype')
 		local clients = vim.lsp.get_active_clients()
 		if next(clients) == nil then
