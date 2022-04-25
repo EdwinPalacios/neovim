f840bc9f167e60d51c8916b3c71a490dfef16d11
EdwinPalacios <epalacios13@outlook.com>
2022-04-25 14:31:19 -0500

    feat(harpoon): modified keymaps

--- lua/edpa/harpoon.lua
+++ lua/edpa/harpoon.lua
@@ -8,12 +8,6 @@ harpoon.setup {}
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true, expr = false }
 
-keymap('n', '<leader>ss', require("harpoon.ui").toggle_quick_menu, opts)
-keymap('n', '<leader>sa', require("harpoon.mark").add_file, opts)
-keymap('n', '<leader><leader>1', function() require("harpoon.term").gotoTerminal(1) end, opts)
-keymap('n', '<leader><leader>2', function() require("harpoon.term").gotoTerminal(2) end, opts)
-keymap('n', '<leader><leader>3', function() require("harpoon.term").gotoTerminal(3) end, opts)
-
-for i = 1, 5 do
-  keymap('n', string.format("<leader>%s", i), function() require("harpoon.ui").nav_file(i) end, opts)
-end
+keymap('n', '<leader>h', ":lua require('harpoon.mark').add_file()<CR>", opts)
+keymap('n', '<leader>m', ":Telescope harpoon marks<CR>", opts)
+keymap('n', '<leader>mm', ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
