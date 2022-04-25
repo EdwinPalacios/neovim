7efc52eb7434cf428ca843191062c7ccab635976
EdwinPalacios <epalacios13@outlook.com>
2022-04-25 14:37:04 -0500

    feat(harpoon): add nav

--- lua/edpa/harpoon.lua
+++ lua/edpa/harpoon.lua
@@ -8,6 +8,9 @@ harpoon.setup {}
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true, expr = false }
 
-keymap('n', '<leader>h', ":lua require('harpoon.mark').add_file()<CR>", opts)
-keymap('n', '<leader>m', ":Telescope harpoon marks<CR>", opts)
-keymap('n', '<leader>mm', ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+keymap('n', '<leader>ha', ":lua require('harpoon.mark').add_file()<CR>", opts)
+keymap('n', '<leader>ht', ":Telescope harpoon marks<CR>", opts)
+keymap('n', '<leader>hm', ":lua require('harpoon.ui').toggle_quick_menu()<CR>", opts)
+
+keymap('n', '<leader>hj', ":lua require('harpoon.ui').nav_prev()<CR>", opts)
+keymap('n', '<leader>hl', ":lua require('harpoon.ui').nav_next()<CR>", opts)
