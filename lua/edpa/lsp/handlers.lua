c69c794ddc2e3fdc594923d3177d5345cdb69f94
EdwinPalacios <epalacios13@outlook.com>
2022-05-04 15:45:50 -0500

    fix(plugins): fix lsp highligh with illuminate

--- lua/edpa/lsp/handlers.lua
+++ lua/edpa/lsp/handlers.lua
@@ -48,23 +48,9 @@ M.setup = function()
 end
 
 local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
-  if client.resolved_capabilities.document_highlight then
-    local present, illuminate = pcall(require, "illuminate")
-    if present then
-      illuminate.on_attach(client)
-    else
-      vim.api.nvim_exec(
-        [[
-          augroup lsp_document_highlight
-            autocmd! * <buffer>
-            autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-            autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-          augroup END
-        ]],
-        false
-      )
-    end
+  local status_ok, illuminate = pcall(require, "illuminate")
+  if status_ok then
+    illuminate.on_attach(client)
   end
 end
 
